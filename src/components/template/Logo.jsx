a615a6ed2c340c266491e5c5816aab2bdbb08eb4
Marcus Vinicius <mvinicius@facilitabots.com>
2020-07-21 18:25:29 -0300

    first commit

---
+++ src/components/template/Logo.jsx
@@ -0,0 +1,10 @@
+import './Logo.css'
+//import logo from '../../assets/imgs/logo192.png'
+import React from 'react'
+
+export default props =>
+<aside className="">
+    <a href="/" className="logo">
+        <img src="" alt="logo"/>
+    </a>
+</aside>
